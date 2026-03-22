b0b7ec43fccd278c66adae1e2d8aa806f13ce897
Dylan Broady <DylanHunterBroady@gmail.com>
2026-03-22 14:42:10 -0400

    Delete book directory

--- book/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Stylized order book and fill models."""
-
-from execution_engine.book.fill_model import FillModel
-from execution_engine.book.lob_state import LOBState
-
-__all__ = ["FillModel", "LOBState"]
