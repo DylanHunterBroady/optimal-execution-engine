0e9b2dc9289959b3d4b5141b366b94721eb01e00
Dylan Broady <DylanHunterBroady@gmail.com>
2026-03-22 14:42:56 -0400

    Delete simulation directory

--- simulation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Simulation runners."""
-
-from execution_engine.simulation.backtest_runner import run_config_directory
-from execution_engine.simulation.monte_carlo import run_monte_carlo
-from execution_engine.simulation.scenario_runner import run_strategy_suite
-from execution_engine.simulation.simulator import ExecutionSimulator
-
-__all__ = ["ExecutionSimulator", "run_config_directory", "run_monte_carlo", "run_strategy_suite"]
